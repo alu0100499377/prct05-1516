9a1aab707fd9c9b5a12ddf4f552aeb0ada5e6b41
Gabi <alu0100499377@ull.edu.es>
2015-10-27 14:40:28 +0100

    creada clase Fraccion

---
+++ lib/racional.rb
@@ -0,0 +1,93 @@
+class Fraccion
+
+	def initialize(n, d)
+		@num = n
+		@denom = d
+	end
+
+	def to_s
+		"(#{@num}/#{@denom})"
+	end
+
+	#maximo comun divisor
+	def gcd(u, v)
+		u, v = u.abs, v.abs
+		while v != 0
+			u, v = v, u % v
+		end
+		u
+	end
+
+	#SUMA
+	def suma(n, d)
+		aux_d = @denom*d
+		aux_n1 = (aux_d/@denom)*@num
+		aux_n2 = (aux_d/d)*n
+		aux_n = aux_n1+aux_n2
+
+		#reduzco la expresion a la minima
+		max = gcd(aux_n, aux_d)
+		@denominador = aux_d/max
+		@numerador = aux_n/max
+
+		fraccion = Fraccion.new(@numerador, @denominador)
+		return fraccion
+	end
+
+	#RESTA
+	def resta(n, d)
+		aux_d = @denom*d
+		aux_n1 = (aux_d/@denom)*@num
+		aux_n2 = (aux_d/d)*n
+		aux_n = aux_n1-aux_n2
+
+		#reduzco el resultado a la minima expresion
+		max = gcd(aux_n, aux_d)
+		@denominador = aux_d/max
+		@numerador = aux_n/max
+		
+		fraccion = Fraccion.new(@numerador, @denominador)
+		return fraccion
+	end
+
+	#MULTIPLICACION
+	def mul(n, d)
+		#multiplico y reduzco el resultado a la minima expresion
+		aux_n = n*@num
+		aux_d = d*@denom
+		max = gcd(aux_n, aux_d)
+		@denominador = aux_d/max
+		@numerador = aux_n/max
+		fraccion = Fraccion.new(@numerador, @denominador)
+		return fraccion
+	end
+
+	#DIVICION
+	def div(n, d)
+		#divido y reduzco el resultado a la minima expresion
+		aux_d = n*@denom
+		aux_n = d*@num
+		max = gcd(aux_n, aux_d)
+		@denominador = aux_d/max
+		@numerador = aux_n/max
+		fraccion = Fraccion.new(@numerador, @denominador)
+		return fraccion
+
+	end
+end #end clase
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
