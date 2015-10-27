3609373be5ecac43e7bdaa35fec44a4df4d0b222
Gabi <alu0100499377@ull.edu.es>
2015-10-27 15:31:14 +0100

    solucionar error en test

--- test/tc_racional.rb
+++ test/tc_racional.rb
@@ -1,4 +1,4 @@
-require 'lib/racional.rb'
+require 'lib/racional'
 require 'test/unit'
 
 class TestFraccion < Test::Unit::TestCase
@@ -6,14 +6,14 @@ class TestFraccion < Test::Unit::TestCase
 		assert_equal("(1/3)", Fraccion.new(1,3).to_s)
 		assert_equal("(2/5)", Fraccion.new(2,5).to_s)
 	end
-
+	
 	def test_suma
-		assert_equal("(11/15)", Fraccion.new(1,3).suma	(2,5).to_s)
+		assert_equal("(11/15)", Fraccion.new(1,3).suma(2,5).to_s)
 	end
 
 	#este debe fallar
-	def test_suma
+	def test_suma_fail
 		assert_equal("(5/6)", Fraccion.new(1,3).div(2,5))
 	end
-	
+
 end
