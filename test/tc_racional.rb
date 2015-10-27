bda633478bb830f5ce9d80f59e8dd92803d492f9
Gabi <alu0100499377@ull.edu.es>
2015-10-27 15:06:28 +0100

    test resta y suma

--- test/tc_racional.rb
+++ test/tc_racional.rb
@@ -7,5 +7,13 @@ class TestFraccion < Test::Unit::TestCase
 		assert_equal("(2/5)", Fraccion.new(2,5).to_s)
 	end
 
+	def test_suma
+		assert_equal("(11/15)", Fraccion.new(1,3).suma	(2,5).to_s)
+	end
+
+	#este debe fallar
+	def test_suma
+		assert_equal("(5/6)", Fraccion.new(1,3).div(2,5))
+	end
 	
 end
